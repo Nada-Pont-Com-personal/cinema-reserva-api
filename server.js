212d730db7957e4cb7b3931ed90c9f3bb9f1045a
Nada.pont.com <nada.pont.com@gmail.com>
2022-12-04 23:48:47 -0300

    sessao

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 import express from "express";
+import session from "express-session";
 
 const port = process.env.port || 5000;
 
 const app = express();
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
 
 app.get("/", (req, res, next) => {
   res.send("ok");
